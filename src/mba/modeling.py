9aaac4c2f69a6c948f3235d172c162622b545ced
AndBoyS <andboyxd@gmail.com>
2024-12-16 16:00:34 +0300

    more mba steps

--- src/mba/modeling.py
+++ src/mba/modeling.py
@@ -1,21 +1,31 @@
-from collections.abc import Iterable
-from typing import Self
+import itertools
+import re
+from collections.abc import Iterable, Iterator
+from functools import partial
+from typing import Any, Self
 
 import numpy as np
 import pandas as pd
-from catboost import CatBoostClassifier
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.impute import SimpleImputer
-from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import f1_score
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
 
 from src.mba.const import TARGET
 
 
 def get_xy(data: pd.DataFrame) -> tuple[pd.DataFrame, pd.Series]:
     return data.drop(columns=TARGET), data[TARGET]
+
+
+class DataFramer(BaseEstimator, TransformerMixin):
+    def __init__(self, features: Iterable[str]) -> None:
+        self.features = list(features)
+
+    def fit(self, X: np.ndarray, y: np.ndarray | None = None) -> Self:
+        return self
+
+    def transform(self, X: np.ndarray) -> pd.DataFrame:
+        return pd.DataFrame(X, columns=self.features)
 
 
 class ManualFeatureSelector(BaseEstimator, TransformerMixin):
@@ -27,6 +37,35 @@ class ManualFeatureSelector(BaseEstimator, TransformerMixin):
 
     def transform(self, X: pd.DataFrame) -> pd.DataFrame:
         return X[self.features]
+
+
+class TextFeaturesExtractor(BaseEstimator, TransformerMixin):
+    def __init__(self, text_feat: str) -> None:
+        self.text_feat = text_feat
+
+    def fit(self, X: pd.DataFrame, y: pd.Series | None = None) -> Self:
+        return self
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        def regex_counter(s: str, ptrn: str) -> int:
+            return len(re.findall(ptrn, s))
+
+        text = X[self.text_feat]
+        X[f"{self.text_feat}_exc"] = text.str.count("!")
+        X[f"{self.text_feat}_high"] = text.apply(
+            partial(regex_counter, ptrn=r"(?<!not\s)(high|substantial|best|prosper|wealth|great|successful)")
+        )
+        X[f"{self.text_feat}_medium"] = text.apply(
+            partial(regex_counter, ptrn=r"(?<!not\s)(medium|solid|confiden|\sreliable|\sbalance|excit)")
+        )
+        X[f"{self.text_feat}_modest"] = text.apply(partial(regex_counter, ptrn=r"(modest)"))
+        X[f"{self.text_feat}_stable"] = text.apply(
+            partial(regex_counter, ptrn=r"(?<!not\s)(planning|stability|\sstable|commited)")
+        )
+        X[f"{self.text_feat}_meh"] = text.apply(
+            partial(regex_counter, ptrn=r"(rocky|unsure|rough|struggl|low|not sure)")
+        )
+        return X
 
 
 class ObjectToStringTransformer(BaseEstimator, TransformerMixin):
@@ -51,3 +90,16 @@ def print_metrics(y_true: np.ndarray | pd.DataFrame, y_pred: np.ndarray | pd.Dat
     print(f"Accuracy: {acc:.3f}")
     f1 = f1_score(y_pred=y_pred, y_true=y_true)
     print(f"f1: {f1:.3f}")
+
+
+def grid_search(params_dict: dict[str, Any]) -> Iterator[dict[str, Any]]:
+    param_combs = itertools.product(*list(params_dict.values()))
+    param_names = list(params_dict)
+
+    for param_vals in param_combs:
+        config_kwargs: dict[str, Any] = {}
+
+        for param_name, param_val in zip(param_names, param_vals):
+            config_kwargs[param_name] = param_val
+
+        yield config_kwargs
--- src/mba/vis.py
+++ src/mba/vis.py
@@ -9,29 +9,29 @@ from sklearn.ensemble import IsolationForest
 from src.mba.const import TARGET
 
 
-def get_num_cols(loan_data: pd.DataFrame) -> list[str]:
+def get_float_cols_for_vis(loan_data: pd.DataFrame) -> list[str]:
     cols = loan_data.select_dtypes(exclude=object).columns
     return [c for c in cols if loan_data[c].nunique() >= 20]
 
 
-def get_cat_cols(loan_data: pd.DataFrame, for_plot: bool = False) -> list[str]:
-    num_cols = get_num_cols(loan_data)
-    cat_cols = list(set(loan_data.columns) - set(num_cols))
+def get_cat_cols_for_vis(loan_data: pd.DataFrame, for_plot: bool = False) -> list[str]:
+    float_cols = get_float_cols_for_vis(loan_data)
+    cat_cols = list(set(loan_data.columns) - set(float_cols))
     if for_plot:
         cat_cols = [c for c in cat_cols if loan_data[c].nunique() < 20]
     return cat_cols
 
 
 def plot_hist(loan_data: pd.DataFrame) -> None:
-    num_cols = get_num_cols(loan_data)
-    loan_data[num_cols].hist(bins=60, figsize=(7, 7))
+    float_cols = get_float_cols_for_vis(loan_data)
+    loan_data[float_cols].hist(bins=60, figsize=(7, 7))
     plt.suptitle("Распределение числовых признаков")
     plt.show()
 
 
 def plot_scatter(loan_data: pd.DataFrame) -> None:
-    num_cols = get_num_cols(loan_data)
-    g = sns.PairGrid(loan_data, x_vars=num_cols, y_vars=num_cols)
+    float_cols = get_float_cols_for_vis(loan_data)
+    g = sns.PairGrid(loan_data, x_vars=float_cols, y_vars=float_cols)
     g.map_lower(sns.scatterplot)
     plt.suptitle("Точечные графики числовых признаков")
     plt.tight_layout()
@@ -39,14 +39,14 @@ def plot_scatter(loan_data: pd.DataFrame) -> None:
 
 
 def print_value_counts(loan_data: pd.DataFrame) -> None:
-    cat_cols = get_cat_cols(loan_data)
+    cat_cols = get_cat_cols_for_vis(loan_data)
     for col in cat_cols:
         print(loan_data[col].value_counts())
         print()
 
 
 def plot_categorical_distributions(loan_data: pd.DataFrame) -> None:
-    cat_feats = get_cat_cols(loan_data, for_plot=True)
+    cat_feats = get_cat_cols_for_vis(loan_data, for_plot=True)
 
     num_cols = 2
     num_rows = (len(cat_feats) + num_cols - 1) // num_cols
@@ -64,7 +64,7 @@ def plot_categorical_distributions(loan_data: pd.DataFrame) -> None:
 
 
 def visualize_cat_and_target(loan_data: pd.DataFrame) -> None:
-    cat_feats = get_cat_cols(loan_data, for_plot=True)
+    cat_feats = get_cat_cols_for_vis(loan_data, for_plot=True)
     num_cols = 2
     num_rows = (len(cat_feats) + num_cols - 1) // num_cols
 
@@ -86,8 +86,8 @@ def get_anomaly_mask(loan_data: pd.DataFrame, col: str) -> np.ndarray:
 
 
 def plot_anomalies(loan_data: pd.DataFrame, bins: int = 100) -> None:
-    num_cols = get_num_cols(loan_data)
-    for col in num_cols:
+    float_cols = get_float_cols_for_vis(loan_data)
+    for col in float_cols:
         anomaly_mask = get_anomaly_mask(loan_data, col)
 
         plt.figure(figsize=(10, 6))
