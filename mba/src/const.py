647da47f55758614ca7ff79d9eac310c25080039
AndBoyS <andboyxd@gmail.com>
2025-09-27 11:28:37 +0300

    edit data to have some no collateral

--- mba/src/const.py
+++ mba/src/const.py
@@ -1,6 +1,13 @@
 from pathlib import Path
 
-DATA_PATH = Path("data/loan_data.csv")
+REPO_DIR = Path(__file__).parents[2]
+
+DATA_PRE_EDIT_DIR = REPO_DIR / "mba/data_pre_edit"
+DATA_DIR = REPO_DIR / "mba/data"
+TRAIN_DATA_PATH = DATA_DIR / "loan_data.csv"
+TRAIN_DATA_XLSX_PATH = TRAIN_DATA_PATH.with_suffix(".xlsx")
+TEST_DATA_PATH = DATA_DIR / "test_data.csv"
+
 TARGET = "дефолт"
 RATE = 0.1
 NUM_MONTHS = 12
---
+++ mba/src/dataset_edits.py
@@ -0,0 +1,33 @@
+"""Originally dataset was created using project/create_dataset.py, we modify it there to be more interesting (since all records had collateral, bank had no way to lose money)"""
+
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+
+from mba.src.const import DATA_DIR, DATA_PRE_EDIT_DIR, TEST_DATA_PATH, TRAIN_DATA_PATH, TRAIN_DATA_XLSX_PATH
+
+NON_COLLATERAL_RATE = 0.3
+
+
+def main() -> None:
+    DATA_DIR.mkdir(exist_ok=True)
+    edit_data(input_path=DATA_PRE_EDIT_DIR / "loan_data.csv", output_path=TRAIN_DATA_PATH)
+    edit_data(input_path=DATA_PRE_EDIT_DIR / "test_data.csv", output_path=TEST_DATA_PATH)
+    # For convenience of students
+    data = pd.read_csv(TRAIN_DATA_PATH)
+    data.to_excel(TRAIN_DATA_XLSX_PATH)
+
+
+def edit_data(input_path: Path, output_path: Path) -> None:
+    data = pd.read_csv(input_path)
+    rng = np.random.default_rng(42)
+    size = data.shape[0]
+    non_collateral_mask = rng.choice(size, replace=False, size=int(size * NON_COLLATERAL_RATE))
+    data.loc[non_collateral_mask, "прямой_залог"] = 0
+    data.loc[non_collateral_mask, "тип_залога"] = np.nan
+    data.to_csv(output_path, index=False)
+
+
+if __name__ == "__main__":
+    main()
